5fe637eb151a80750669f649777dd4c2950e59a1
Mahdi salimkhani <55203977+mah5001@users.noreply.github.com>
2022-08-22 09:50:04 +0430

    1

--- script.js
+++ script.js
@@ -9,14 +9,13 @@ var a = aa.value;
 var b = bb.value;
 var c = cc.value;
 var d= dd.value;
-var Q = (3*a*c - b*b)/(9*a*a);
-var R = ((9 * a * b * c) - (27 * a * a * d) - (2 * b * b * b)) / (54 * a * a * a);
-var S = Math.cbrt(R + Math.sqrt(Q * Q * Q + R * R));
-var T = Math.cbrt(R - Math.sqrt(Q * Q * Q + R * R));
-var x1 = Math.round(((S + T - (b / 3 * a)) + Number.EPSILON) * 100) / 100;
-var x2 = Math.round(((-(S + T) / 2 - (b / 3 * a)) + Number.EPSILON) * 100) / 100 + "+" + Math.round(((Math.sqrt(3) / 2) * (S - T) + Number.EPSILON) * 100)/100+ "i";
-var x3 = Math.round(((-(S + T) / 2 - (b / 3 * a)) + Number.EPSILON) * 100) / 100 + "-" + (Math.round(((Math.sqrt(3) / 2) * (S - T) + Number.EPSILON) * 100) / 100 )+ "i";
-if(x1==NaN){
+//var Q = (3*a*c - b*b)/(9*a*a);
+//var R = ((9 * a * b * c) - (27 * a * a * d) - (2 * b * b * b)) / (54 * a * a * a);
+//var S = Math.cbrt(R + Math.sqrt(Q * Q * Q + R * R));
+//var T = Math.cbrt(R - Math.sqrt(Q * Q * Q + R * R));
+//var x1 = Math.round(((S + T - (b / 3 * a)) + Number.EPSILON) * 100) / 100;
+//var x2 = Math.round(((-(S + T) / 2 - (b / 3 * a)) + Number.EPSILON) * 100) / 100 + "+" + Math.round(((Math.sqrt(3) / 2) * (S - T) + Number.EPSILON) * 100)/100+ "i";
+//var x3 = Math.round(((-(S + T) / 2 - (b / 3 * a)) + Number.EPSILON) * 100) / 100 + "-" + (Math.round(((Math.sqrt(3) / 2) * (S - T) + Number.EPSILON) * 100) / 100 )+ "i";
 var a = b/a;
 var b = c/a;
 var c = d/a;
@@ -26,17 +25,13 @@ var delta = (27*q*q+ 4*p*p*p)/108;
 console.table(q,p,Math.round(delta * 1000)/1000);
     if(Math.round(delta * 1000)/1000>0){
     console.log("is Positive");
-    alert(Math.cbrt(-q/2 + Math.sqrt(delta)) + Math.cbrt(-q/2 - Math.sqrt(delta)) - a/3);}
+    alert(Math.cbrt("X1:" +-q/2 + Math.sqrt(delta)) + Math.cbrt(-q/2 - Math.sqrt(delta)) - a/3);}
     if(Math.round(delta * 1000)/1000==0){
     console.log("is zero");
-    alert(-2 * Math.cbrt(q/2)-a/3 , Math.cbrt(q/2)-a/3);}
+    alert("X1:" +-2 * Math.cbrt(q/2)-a/3 +"X2:" +Math.cbrt(q/2)-a/3);}
     if(Math.round(delta * 1000)/1000<0){
     console.log("is negative");}
-
-} else{
-alert("X1 : " + x1 + " , x2 : " + x2 + " , x3 : " + x3);
-} 
     
-}} 
+}
 
 btn.addEventListener("click",Calculate);
